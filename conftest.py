9498edbaf2d4076d6984660003ec3736243e237c
juggler108 <juggler108@gmail.com>
2023-02-15 18:26:41 +0400

    changed assert

--- conftest.py
+++ conftest.py
@@ -8,6 +8,26 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru", help="Choose the language!")
 
 
+@pytest.fixture(scope="function")
+def browser(request):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print("\nstart chrome browser for test..")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(3)
+
+    driver.get(link)
+    sleep(5)
+    yield driver
+
+    print("\nquit browser..")
+    driver.quit()
+
+
 # @pytest.fixture(scope="function")
 # def browser(request):
 #     language = request.config.getoption("language")
@@ -23,22 +43,3 @@ def pytest_addoption(parser):
 #
 #     print("\nquit browser..")
 #     driver.quit()
-
-@pytest.fixture(scope="function")
-def browser(request):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    user_language = request.config.getoption("language")
-
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-
-    print("\nstart chrome browser for test..")
-    driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(3)
-
-    driver.get(link)
-    sleep(10)
-    yield driver
-
-    print("\nquit browser..")
-    driver.quit()
--- test_items.py
+++ test_items.py
@@ -1,6 +1,5 @@
 
 
 def test_guest_should_see_button_add_to_basket(browser):
-    basket_button = browser.find_element(by="class name", value="btn-add-to-basket")
 
-    assert basket_button, "The basket button is not founded!"
+    assert browser.find_elements(by="class name", value="btn-add-to-basket"), "The basket button is not founded!"
